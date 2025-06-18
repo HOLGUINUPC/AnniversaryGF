dcea5809d2bd9ca8c71681dbe4e393151303a59c
HOLGUINUPC <u202220250@upc.edu.pe>
2025-06-17 21:01:13 -0500

    feat: added routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { BotonesInteractivosComponent } from './botones-interactivos/botones-interactivos';
+import { LoveView } from './love-view/love-view';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'init', pathMatch: 'full' }, // redirecciona a init
+  { path: 'init', component: BotonesInteractivosComponent }, // init carga el componente
+  { path: 'amor', component: LoveView } // nueva ruta
+];
