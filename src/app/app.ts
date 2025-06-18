6ba845b9a43c06f3c46fcbac7470be6249666b7c
HOLGUINUPC <u202220250@upc.edu.pe>
2025-06-17 21:01:30 -0500

    feat: added component ts

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component, inject} from '@angular/core';
+import {Router, RouterOutlet} from '@angular/router';
+import {BotonesInteractivosComponent} from './botones-interactivos/botones-interactivos';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.html',
+  standalone: true,
   styleUrl: './app.css'
 })
 export class App {
+
+
+  private router = inject( Router );
+
   protected title = 'untitled28';
 }
