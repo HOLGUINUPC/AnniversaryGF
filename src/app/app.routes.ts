3921483bdda780cf9926a0d682fa096ce80f9246
HOLGUINUPC <u202220250@upc.edu.pe>
2025-06-17 22:27:49 -0500

    feat: added routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,12 @@
 import { Routes } from '@angular/router';
 import { BotonesInteractivosComponent } from './botones-interactivos/botones-interactivos';
 import { LoveView } from './love-view/love-view';
+import {Palabras} from './palabras/palabras';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'init', pathMatch: 'full' }, // redirecciona a init
   { path: 'init', component: BotonesInteractivosComponent }, // init carga el componente
-  { path: 'amor', component: LoveView } // nueva ruta
+  { path: 'amor', component: LoveView }, // nueva ruta
+  { path: 'palabras', component: Palabras } // nueva ruta
+
 ];
